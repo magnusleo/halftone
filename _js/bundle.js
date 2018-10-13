5cc193eda4e2e8b75d74cbee8c4d64319b9280de
Magnus Leo <2414388+magnusleo@users.noreply.github.com>
2018-10-13 20:48:14 +0200

    Reading data from a test image

--- _js/bundle.js
+++ _js/bundle.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hello world");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+if (ctx) {
+    var image_1 = new Image();
+    image_1.src = "example.png";
+    image_1.onload = function () {
+        ctx.drawImage(image_1, 0, 0);
+        var imageData = ctx.getImageData(0, 0, image_1.width, image_1.height);
+        console.log(imageData);
+    };
+}
+else {
+    throw new Error("Canvas context not found");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+if (ctx) {
+  const image = new Image();
+  image.src = "example.png";
+  image.onload = function() {
+    ctx.drawImage(image, 0, 0);
+    const imageData = ctx.getImageData(0, 0, image.width, image.height);
+    console.log(imageData);
+  };
+} else {
+  throw new Error("Canvas context not found");
+}
