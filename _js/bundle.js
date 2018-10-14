654aec091a20e31ca6644f43740e854401722d25
Magnus Leo <2414388+magnusleo@users.noreply.github.com>
2018-10-14 09:11:26 +0200

    Working upscaling

--- _js/bundle.js
+++ _js/bundle.js
@@ -1,4 +1,10 @@
 "use strict";
+var svgNameSpace = "http://www.w3.org/2000/svg";
+var svg = document.createElementNS(svgNameSpace, "svg");
+svg.setAttribute("viewBox", "0 0 210 340");
+svg.setAttribute("height", "210");
+svg.setAttribute("width", "340");
+document.getElementsByTagName("body")[0].appendChild(svg);
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 if (ctx) {
@@ -7,7 +13,22 @@ if (ctx) {
     image_1.onload = function () {
         ctx.drawImage(image_1, 0, 0);
         var imageData = ctx.getImageData(0, 0, image_1.width, image_1.height);
-        console.log(imageData);
+        var width = imageData.width;
+        var radius = 5;
+        for (var i = 0; i < imageData.data.length; i += 4) {
+            var pixelNum = i / 4;
+            var x = pixelNum % width;
+            var y = Math.floor(pixelNum / width);
+            var r = imageData.data[i];
+            var g = imageData.data[i + 1];
+            var b = imageData.data[i + 2];
+            var pixel = document.createElementNS(svgNameSpace, "circle");
+            pixel.setAttribute("r", radius.toString());
+            pixel.setAttribute("cx", (x * radius * 2).toFixed(0));
+            pixel.setAttribute("cy", (y * radius * 2).toFixed(0));
+            pixel.setAttribute("fill", "rgb(" + r + "," + g + "," + b + ")");
+            svg.appendChild(pixel);
+        }
     };
 }
 else {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+const svgNameSpace = "http://www.w3.org/2000/svg";
+const svg = document.createElementNS(svgNameSpace, "svg");
+svg.setAttribute("viewBox", "0 0 210 340");
+svg.setAttribute("height", "210");
+svg.setAttribute("width", "340");
+document.getElementsByTagName("body")[0].appendChild(svg);
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 if (ctx) {
@@ -6,7 +13,24 @@ if (ctx) {
   image.onload = function() {
     ctx.drawImage(image, 0, 0);
     const imageData = ctx.getImageData(0, 0, image.width, image.height);
-    console.log(imageData);
+    const width = imageData.width;
+    const radius = 5;
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      const pixelNum = i / 4;
+      const x = pixelNum % width;
+      const y = Math.floor(pixelNum / width);
+      const r = imageData.data[i];
+      const g = imageData.data[i + 1];
+      const b = imageData.data[i + 2];
+
+      const pixel = document.createElementNS(svgNameSpace, "circle");
+      pixel.setAttribute("r", radius.toString());
+      pixel.setAttribute("cx", (x * radius * 2).toFixed(0));
+      pixel.setAttribute("cy", (y * radius * 2).toFixed(0));
+      pixel.setAttribute("fill", `rgb(${r},${g},${b})`);
+      svg.appendChild(pixel);
+    }
   };
 } else {
   throw new Error("Canvas context not found");
