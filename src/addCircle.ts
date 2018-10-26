88672c0a011fa5fcfdb4767fa0eba55b78ab2291
Magnus Leo <2414388+magnusleo@users.noreply.github.com>
2018-10-26 19:58:23 +0200

    Use a more reasonable precision for SVG elements

--- src/addCircle.ts
+++ src/addCircle.ts
@@ -21,9 +21,12 @@ export default function addCircle(
   lightness: number
 ): void {
   const pixel = document.createElementNS(svgNameSpace, "circle");
-  pixel.setAttribute("r", radius.toString());
+  pixel.setAttribute("r", radius.toPrecision(3));
   pixel.setAttribute("cx", x.toFixed(0));
   pixel.setAttribute("cy", y.toFixed(0));
-  pixel.setAttribute("fill", `hsl(${hue}, ${saturation}%, ${lightness}%)`);
+  pixel.setAttribute(
+    "fill",
+    `hsl(${hue}, ${saturation.toFixed(0)}%, ${lightness.toFixed(0)}%)`
+  );
   svg.appendChild(pixel);
 }
